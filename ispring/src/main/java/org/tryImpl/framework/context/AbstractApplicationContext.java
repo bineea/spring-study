727610936954d0ce654688ded568fe8c7d61f066
bineea <33547731+bineea@users.noreply.github.com>
2021-08-22 17:20:29 +0800

    自定义spring

--- ispring/src/main/java/org/tryImpl/framework/context/AbstractApplicationContext.java
+++ ispring/src/main/java/org/tryImpl/framework/context/AbstractApplicationContext.java
@@ -1,23 +1,28 @@
 package org.tryImpl.framework.context;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public abstract class AbstractApplicationContext implements BeanFactory {
 
     protected Map<String, Object> singletonObjects = new HashMap<>();
 
-    protected Map<String, Object> singletonFactories = new HashMap<>();
-
     protected Map<String, Object> earlySingletonObjects = new HashMap<>();
+
+    protected Map<String, Object> singletonFactories = new HashMap<>();
 
     protected Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
     protected List<String> beanNameList = new ArrayList<>();
 
     protected Object keepSafeOperation = new Object();
+
+    protected Set<String> singletonsCurrentlyInCreation = new HashSet<>();
+
+    @Override
+    public Object getBean(String beanName) {
+        return singletonObjects.get(beanName);
+    }
 
     protected final void create() {
         synchronized (this.keepSafeOperation) {
@@ -34,10 +39,43 @@ public abstract class AbstractApplicationContext implements BeanFactory {
     /**
      * 创建单例bean
      */
-    protected abstract void createSingletonBean();
+    protected void createSingletonBean() {
+        for(String beanName : beanNameList) {
+            Object obj = getSingleton(beanName);
+            if(obj == null) {
 
-    @Override
-    public Object getBean(String beanName) {
-        return singletonObjects.get(beanName);
+            }
+
+
+            //创建代理
+        }
     }
+
+    private Object getSingleton(String beanName) {
+        Object obj = singletonObjects.get(beanName);
+        if(obj == null) {
+            obj = earlySingletonObjects.get(beanName);
+            if(obj == null) {
+                obj = singletonFactories.get(beanName);
+            }
+        }
+        return obj;
+    }
+
+    private Object doCreateBeanInstance(String beanName) {
+        try {
+            Object sampleInstance = beanDefinitionMap.get(beanName).getBeanClass().getDeclaredConstructor().newInstance();
+            return sampleInstance;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
