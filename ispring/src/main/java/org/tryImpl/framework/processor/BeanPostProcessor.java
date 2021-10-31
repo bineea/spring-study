5cb8984b3969b9858b71bf091d9a122c638ec52e
bineea <33547731+bineea@users.noreply.github.com>
2021-10-31 16:50:02 +0800

    增加备注

--- ispring/src/main/java/org/tryImpl/framework/processor/BeanPostProcessor.java
+++ ispring/src/main/java/org/tryImpl/framework/processor/BeanPostProcessor.java
@@ -2,10 +2,22 @@ package org.tryImpl.framework.processor;
 
 public interface BeanPostProcessor {
 
+    /**
+     * Bean对象初始化前执行方法
+     * @param bean
+     * @param beanName
+     * @return
+     */
     default Object postProcessBeforeInitialization(Object bean, String beanName) {
         return bean;
     }
 
+    /**
+     * Bean对象初始化后执行方法
+     * @param bean
+     * @param beanName
+     * @return
+     */
     default Object postProcessAfterInitialization(Object bean, String beanName) {
         return bean;
     }
