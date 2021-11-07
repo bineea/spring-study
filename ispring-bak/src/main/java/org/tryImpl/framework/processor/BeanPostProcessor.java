93a67654120a8013cad53ecbf2b9082467a8b0c7
bineea <33547731+bineea@users.noreply.github.com>
2021-11-07 23:24:24 +0800

    调整pom依赖

--- ispring-bak/src/main/java/org/tryImpl/framework/processor/BeanPostProcessor.java
+++ ispring-bak/src/main/java/org/tryImpl/framework/processor/BeanPostProcessor.java
@@ -1,16 +1,12 @@
 package org.tryImpl.framework.processor;
 
-import com.sun.istack.internal.Nullable;
-
 //Bean的后置处理器
 public interface BeanPostProcessor {
 
-    @Nullable
     default Object postProcessBeforeInitialization(Object bean, String beanName) {
         return bean;
     }
 
-    @Nullable
     default Object postProcessAfterInitialization(Object bean, String beanName) {
         return bean;
     }
