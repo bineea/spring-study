a7219c8bfccbc87f36036f058602ba42eb3df3e2
bineea <33547731+bineea@users.noreply.github.com>
2022-03-29 23:21:39 +0800

    解析事务注解方法

--- ispring/src/main/java/org/tryImpl/framework/transaction/DataSourceTransactionManager.java
+++ ispring/src/main/java/org/tryImpl/framework/transaction/DataSourceTransactionManager.java
@@ -2,7 +2,6 @@ package org.tryImpl.framework.transaction;
 
 import javax.sql.DataSource;
 
-//TODO
 public class DataSourceTransactionManager implements PlatformTransactionManager {
 
     private DataSource dataSource;
@@ -15,18 +14,30 @@ public class DataSourceTransactionManager implements PlatformTransactionManager 
         this.dataSource = dataSource;
     }
 
+    public DataSourceTransactionManager(DataSource dataSource) {
+        setDataSource(dataSource);
+        afterPropertiesSet();
+    }
+
+    public void afterPropertiesSet() {
+        if (getDataSource() == null) {
+            throw new IllegalArgumentException("数据源不能为空");
+        }
+    }
+
     @Override
     public TransactionStatus getTransaction(TransactionDefinition transactionDefinition) throws Exception {
+        //TODO
         return null;
     }
 
     @Override
     public void commit(TransactionStatus transactionStatus) throws Exception {
-
+        //TODO
     }
 
     @Override
     public void rollback(TransactionStatus transactionStatus) throws Exception {
-
+        //TODO
     }
 }
